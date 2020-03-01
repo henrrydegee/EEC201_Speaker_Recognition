01b571b637d3aa9484d7b11d88041c469d89cb01
henrrydegee <33557012+henrrydegee@users.noreply.github.com>
2020-03-01 11:14:41 -0800

    Searching for Cluster points cloud

--- main.m
+++ main.m
@@ -11,40 +11,51 @@ M = round(N*2/3); % overlap length for stft()
 %% MAIN
 
 % Step 0: Get file
-[s, fs] = getFile(10);
+[s10, fs10] = getFile(10);
+[s2, fs2] = getFile(2);
 
-% Step 1: Take the stft of signal
-[yst, ystf, ystt] = stft(s, fs, 'Window', hamming(N), 'OverlapLength', M);
-% yst: mxn matrix amplitude output of stft
-% ystf: mx1 frequency output corresponding to stft
-% ystt: nx1 time output corresponding to stft
+%plot(0:length(t)-1, abs(fft(s)))
+%sound(s10, fs10)
+%sound(cropZero(s2), fs2)
+%sound(cropZero(s10), fs10)
+%plotTime(s10, fs10)
+plotTime(cropZero(s10), fs10)
+plotTime(cropZero(s2), fs2)
 
-% plotSpec(ystt, ystf, db(abs(yst))); caxis([-40, max(db(yst(:)))]);
-% xlim([min(ystt), max(ystt)]); ylim([min(ystf), max(ystf)]);
-
-% Step 2: Get mel frequency filter bank
-m = melfb(p, N, fs); % Mel-spaced filter bank
-
-% figure; plot(linspace(0, (fs/2), 129), m');
-% title('Mel-spaced filterbank'); xlabel('Frequency (Hz)');
-
-% Step 3: Take the positive half of the frequency (since symmetry)
-% and matrix multiply the mel filter bank with stft output
-ystcut = yst(128:end, :);
-ystcut = ystcut .* conj(ystcut); % take square
-mfcstuff = m * ystcut; % matrix multiply mel with stft
-
-% plotSpec(ystt, 1:p, db(abs(mfcstuff))); caxis([-40, max(db(mfcstuff(:)))]);
-% xlim([min(ystt), max(ystt)]); ylim([0 p-1]);
-
-% Step 4: Take the log10 of the matrix multiply output
-% and apply dct
-sk = log10(mfcstuff);
-cn = dct(sk);
+%% Start MFCC
+[cn10, ystt10] = mfcc(cropZero(s10), fs10, N, p, M);
+[cn2, ystt2] = mfcc(cropZero(s2), fs2, N, p, M);
 
 % Step 5: Plot the amplitude output of the dct
-plotSpec(ystt, 1:p, cn); caxis([-30 15]);
-xlim([min(ystt), max(ystt)]); ylim([1 p]);
+plotMFCC(ystt10, cn10, p, 10)
+plotMFCC(ystt2, cn2, p, 2)
+
+%% Recognizing Point Clusters through different axis
+
+% Plotting Time vs Amplitude at a given mfc coefficient
+% mfcc_id = 1;
+% figure; plot(ystt2, cn2(mfcc_id,:)', 'x');
+% xlabel('Time (s)'); ylabel('Amplitude');
+% title(strcat( 'Speaker ID: ', int2str(2),...
+%     ' w/ Coefficient: ', int2str(mfcc_id) ));
+
+% Dynamically Plotting mfcc vs Amplitude through time
+% figure;
+% plot(1:p, cn2(:,1), 'x');
+% hold on
+% plot(1:p, cn10(:,1), 'o');
+% for t = 2:length(ystt10)
+%     hold off
+%     plot(1:p, cn2(:,t), 'x');
+%     hold on
+%     plot(1:p, cn10(:,t), 'o');
+%     xlabel('mfcc Coefficient'); ylabel('Amplitude');
+%     ylim([-10, 10]);
+%     legend('Speaker ID: 2', 'Speaker ID: 10');
+%     pause(0.1); hold off
+%     %title(strcat( 'Time: ', int2str(ystt2(t) ) ));
+% end
+
 
 %% File IO Functions
 function [s, fs, t] = getFile(id)
@@ -81,6 +92,13 @@ function plotSpec(x, y, z)
     figure; surf(x, y, z,'EdgeColor','none'); view(0, 90); colorbar;
 end
 
+function plotMFCC(ystt, cn, p, speaker_id)
+    plotSpec(ystt, 1:p, cn); caxis([-30 15]);
+    xlim([min(ystt), max(ystt)]); ylim([1 p]);
+    xlabel('Time (s)'); ylabel('mfc coefficients');
+    title(strcat('Speaker ID: ', int2str(speaker_id)) );
+end
+
 %% Pre-Processing Functions
 function y = ampNorm(s)
     % Normalizes the signal to max amp of 1
@@ -93,6 +111,78 @@ function y = cropZero(s)
     % TODO: tweak sensitivity, possibly calc dB
     
     s2 = round(s, 3); % sensitivity 
-    cri = abs(s2) > 0;
+    cri = abs(s2) > db2mag(-30);
     y = s(find(cri, 1, 'first'):find(cri, 1, 'last')); % crop
+end
+
+%% Signal Generator
+function xn = fseries(f1, f2, n, fs, step)
+% Generates Fourier Series with given frequencies
+% Input:
+% f1 = Starting Frequency
+% f2 = Ending Frequency
+% n = Time Domain / Domain of Signal
+% fs = Sampling Frequency
+% step = Steps between the consecutive frequencies
+% Output:
+% xn = x[n] Fourier Series
+
+    xn = 0;     % Initialization of x[n]
+    for f = f1:step:f2    % Fourier Series frequency
+        xn = xn + sin( 2.*pi.*f.* (n ./fs) ) ;%...
+            %+ cos( 2.*pi.*f.* (n ./fs) );
+        if ( f > 1)
+            xn = xn * 100 ./ (f);
+        end
+    end
+end
+
+%% mfcc function
+function [cn, ystt] = mfcc(s, fs, N, p, M)
+% Calculates the Mel-Frequency Ceptstral Coefficients
+% Inputs:
+% s - Audio vector (assuming 1-channel/mono-channel)
+% fs - Sampling Frequency
+% N - Number of elements in Hamming window for stft()
+% p - Number of filters in the filter bank for melfb
+% M - overlap length for stft()
+%
+% Outputs:
+% cn - Mel-Frequency Ceptstral Coefficients w/ size (p*length(ystt))
+% ystt - Time-domain for STFT(Short-Time Fourier Transform)
+
+% Step 1: Take the stft of signal
+[yst, ystf, ystt] = stft(s, fs, 'Window', hamming(N), 'OverlapLength', M);
+% yst: mxn matrix amplitude output of stft
+% ystf: mx1 frequency output corresponding to stft
+% ystt: nx1 time output corresponding to stft
+
+% plotSpec(ystt, ystf, db(abs(yst))); caxis([-40, max(db(yst(:)))]);
+% xlim([min(ystt), max(ystt)]); ylim([min(ystf), max(ystf)]);
+% xlabel('Time (s)'); ylabel('Frequency (Hz)');
+
+% Step 2: Get mel frequency filter bank
+m = melfb(p, N, fs); % Mel-spaced filter bank
+
+% figure; plot(linspace(0, (fs/2), 129), m');
+% title('Mel-spaced filterbank'); xlabel('Frequency (Hz)');
+
+% Step 3: Take the positive half of the frequency (since symmetry)
+% and matrix multiply the mel filter bank with stft output
+ystcut = yst(128:end, :);
+ystcut = ystcut .* conj(ystcut); % take square
+mfcstuff = m * ystcut; % matrix multiply mel with stft
+
+% plotSpec(ystt, 1:p, db(abs(mfcstuff))); caxis([-40, max(db(mfcstuff(:)))]);
+% xlim([min(ystt), max(ystt)]); ylim([0 p-1]);
+
+% Step 4: Take the log10 of the matrix multiply output
+% and apply dct (Discrete Cosine Transform)
+sk = log10(mfcstuff);
+cn = dct(sk);
+
+% Step 5: Plot the amplitude output of the dct
+%plotSpec(ystt, 1:p, cn); caxis([-30 15]);
+%xlim([min(ystt), max(ystt)]); ylim([1 p]);
+%xlabel('Time (s)'); ylabel('mfc coefficients')
 end
